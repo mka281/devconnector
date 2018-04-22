c8e100ea7c7d815b710e74761812ed306ed5648a
mka <mka281@icloud.com>
2018-04-23 02:25:38 +0300

    Connect to MongoDB with Mongoose

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://devcon-admin:devcon-admin@ds255329.mlab.com:55329/devconnector"
+
+}
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello') )
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
 
 
-const port = process.env.PORT || 2828
-app.listen(port, () => console.log(`Server running on port ${port}`))
+const port = process.env.PORT || 2828;
+app.listen(port, () => console.log(`Server running on port ${port}`));
