b009f063f65531688e1bd6eb6a9ee30d9f2618f9
mka <mka281@icloud.com>
2018-04-26 20:27:56 +0300

    Create route to add education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 // Load validations
 const validateProfileInput = require('../../validation/profile')
 const validateExperienceInput = require('../../validation/experience')
+const validateEducationInput = require('../../validation/education')
 
 // Load Profile model
 const Profile = require('../../models/Profile');
@@ -196,8 +197,37 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
       profile.experience.unshift(newExp);
 
       profile.save().then(profile => res.json(profile))
+    });
+});
 
+// @route   POST api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateEducationInput(req.body)
 
+  // Check validation
+  if (!isValid) {
+    // return any errors with 400 status
+    return res.status(400).json(errors)
+  }
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldOfStudy: req.body.fieldOfStudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      }
+
+      // Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile))
     })
 });
 
