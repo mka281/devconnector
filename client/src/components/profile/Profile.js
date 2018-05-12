55caf0c1e5ccc4e4a302dd9cbbb64ec1494b9184
mka <mka281@icloud.com>
2018-05-12 10:05:10 +0300

    Build Profile Github component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -39,7 +39,9 @@ class Profile extends Component {
             education={profile.education}
             experience={profile.experience}
           />
-          <ProfileGithub />
+          {profile.githubUsername ? (
+            <ProfileGithub username={profile.githubUsername} />
+          ) : null}
         </div>
       );
     }
